3cae919bfc2a9dd5c6bcc5f664bd197d7ea1275f
Shuulker <JosephSparrowRoberts26@gmail.com>
2024-02-28 19:31:21 -0800

    Completed Java Assignment 006

--- src/CanSpell.java
+++ src/CanSpell.java
@@ -1,2 +1,30 @@
 public class CanSpell {
+    public static boolean canSpell(String tiles, String word) {
+        char[] tilesArray = tiles.toCharArray();
+        char[] wordArray = word.toCharArray();
+
+        for (char letter : wordArray) {
+            int index = indexOf(tilesArray, letter);
+            if (index != -1) {
+                tilesArray[index] = '\0';
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static int indexOf(char[] array, char target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(canSpell("quijibo","jib"));
+    }
 }
