d8b078a19878ff51aa63f09cf0f96309609e7288
Eliot Rodriguez <erodriguez314@mycr.redwoods.edu>
2024-02-29 11:38:18 -0800

    Completed Java Lab 006

--- src/CanSpell.java
+++ src/CanSpell.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class CanSpell {
+public static void canSpell (String a, String b){
+if (char i = 0; i>0=0; i++);
+}
+static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("input two strings: ");
+        String a = scanner.next();
+        String b = scanner.next();
+        canSpell(a,b);
+    }
 }
