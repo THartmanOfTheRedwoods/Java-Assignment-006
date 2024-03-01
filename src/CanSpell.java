1885f6581a5be84f9b963a6b1d302695c5b49647
tpedersen42 <pedersen.d.taylor@gmail.com>
2024-03-01 10:34:11 -0800

    Done with assignment 6

--- src/CanSpell.java
+++ src/CanSpell.java
@@ -1,2 +1,28 @@
 public class CanSpell {
+    public static boolean canSpell(String tiles, String word){
+        char[] tileArray = tiles.toCharArray(); //set of tiles
+
+        for (char c : word.toCharArray()) {//iterate through each character
+            boolean found = false;
+
+            for (int i = 0; i < tileArray.length; i++) { //iterate through tiles to find character
+                if (tileArray[i] == c) {
+                    tileArray[i] = ' ';//if found remove from set
+                    found = true;
+                    break;
+
+                }
+            }
+            if (!found) { //if not found there are no available tiles
+                return false;
+            }
+        }
+        return true; //if it can be spelled using the tiles return true
+    }
+
+    public static void main(String[] args) { //tests
+        System.out.println(canSpell("abcdefg", "cab")); //true
+        System.out.println(canSpell("abcdefg", "hill")); //false
+
+    }
 }
