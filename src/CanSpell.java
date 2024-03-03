ac690ecfec94dc141742134f57f6912f5d05d124
Molly-19 <severdiamolly@gmail.com>
2024-03-03 10:50:57 -0800

    Molly Severdia Assignment 006

--- src/CanSpell.java
+++ src/CanSpell.java
@@ -1,2 +1,36 @@
+import java.util.Scanner;
+
 public class CanSpell {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter a list of tiles: ");
+        String t = in.nextLine();
+        System.out.println("Please enter a word: ");
+        String w = in.nextLine();
+
+        boolean spell = canSpell(t, w);
+        if (spell) {
+            System.out.printf("The list of tiles %s can spell the word %s.", t, w);
+        } else {
+            System.out.printf("The list of tiles %s can not spell the word %s.", t, w);
+        }
+    }
+
+    public static boolean canSpell(String tiles, String word) {
+        boolean canSpell = true;
+        int wordLength = word.length();
+        for (int i=0; i<wordLength; i++){
+            int tilesLength = tiles.length();
+            char c = word.charAt(i);
+            int cIndex = tiles.indexOf(c);
+            if (cIndex < 0) {
+                canSpell = false;
+                break;
+            } else {
+                tiles = tiles.substring(0,cIndex) + tiles.substring(cIndex+1, tilesLength);
+            }
+        }
+        return canSpell;
+    }
 }
