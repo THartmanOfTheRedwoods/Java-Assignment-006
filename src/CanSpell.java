06365f6849bb4b1372d6a811e3ebd62c7be223c3
Jaime Ortiz <jaimeortiz0724@gmail.com>
2024-03-02 21:10:34 -0800

    finished assignment 006

--- src/CanSpell.java
+++ src/CanSpell.java
@@ -1,2 +1,19 @@
 public class CanSpell {
+    public static boolean canSpell(String tiles, String word) {
+        int [] letters = new int [26];
+        for (char c : tiles.toCharArray()) {
+            letters[c - 'a']++;
+        }
+        for (char c : word.toCharArray()) {
+            if (letters[c - 'a'] == 0) {
+                return false;
+            }
+            letters[c - 'a'] --;
+        }
+        return true;
+    }
+    public static void main (String[] args) {
+        System.out.println(canSpell("quijibo", "jib"));
+        System.out.println(canSpell("quijibo", "quiz"));
+    }
 }
