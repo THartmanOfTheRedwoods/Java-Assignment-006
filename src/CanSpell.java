b967c79a27ec7c285219fe824c6d3b9c721815d6
jli667 <JennyL137740@gmail.com>
2024-03-03 23:27:54 -0800

    Completed assignment 006

--- src/CanSpell.java
+++ src/CanSpell.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
 public class CanSpell {
+
+    public static boolean canSpell(String word, String word2) {
+        int length = word2.length();
+        for (int i = 0; i < length; i++) {
+            char c = word2.charAt(i);
+            if (frequency(word2, c) > frequency(word, c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static int frequency(String word, char c) {
+        int length2 = word.length();
+        int n = 0;
+        for(int i = 0; i < word.length(); i++) {
+        }
+        return n;
+    }
+    public static void main(String[] strings) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter a word: ");
+         String word = in.nextLine();
+        System.out.println("Enter a another word you would like to test: ");
+         String word2 = in.nextLine();
+
+        System.out.println("canSpell(" + word + "," + word2 + ") --> " + canSpell(word, word2));
+    }
 }
