ec1e8c9e2f7920188aa354251906839c6f47115c
Rigel <rigelcluciani@gmail.com>
2024-03-04 00:22:13 -0800

    3/3/2024 Ry Luciani

--- src/CanSpell.java
+++ src/CanSpell.java
@@ -1,2 +1,34 @@
+
+import java.util.Scanner;
 public class CanSpell {
-}
+
+    public static boolean canSpell(String x, String y) {
+        int[] character = new int[26];
+
+        for (char c : x.toCharArray()) {
+            if (c >= 'a' && c <= 'z') {
+                character[c - 'a']++;
+            }
+        }
+
+        for (char c : y.toCharArray()) {
+            if (c >= 'a' && c <= 'z') {
+                if (character[c - 'a']-- <= 0) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your string of letters");
+        String letters = scanner.nextLine();
+        System.out.println("Enter the word you would like to test");
+        String word = scanner.nextLine();
+
+        System.out.println(canSpell(letters, word));
+        }
+    }
