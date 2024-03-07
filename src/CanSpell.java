0a8ace2eee2f3993fdfca4a662c541c4ae21543d
jcheong641 <jcheong641@mycr.redwoods.edu>
2024-03-06 18:16:59 -0800

    Completed Assignment 06

--- src/CanSpell.java
+++ src/CanSpell.java
@@ -1,2 +1,23 @@
 public class CanSpell {
+    public static void main(String[] args) {
+        System.out.println(canSpell("quijibo", "jib"));
+        System.out.println(canSpell("program", "nope"));
+        System.out.println(canSpell("thanks", "tan"));
+    }
+    public static boolean canSpell(String tile, String word) {
+        if(tile.length() < word.length()) {
+            return false;
+        }
+        for (char c : word.toCharArray()) {
+            int index = tile.indexOf(c);
+
+            if(index == -1) {
+                return false;
+            }
+
+            tile = tile.substring(0, index) + tile.substring(index + 1);
+        }
+        return true;
+
+    }
 }
