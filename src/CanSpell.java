0055e6620378777590a81a1830d50ed9dad7baeb
diego <Diego.car7423@gmail.com>
2024-03-10 22:32:56 -0700

    I just finished assignment 06!

--- src/CanSpell.java
+++ src/CanSpell.java
@@ -1,2 +1,17 @@
 public class CanSpell {
+
+    public static boolean canSpell(String tiles, String word) {
+        if(word.length() > tiles.length()) {return false;}
+        int n = word.length();
+        for(int i=0; i < n; i++) {
+            int index;
+            if( (index = tiles.indexOf(word.charAt(i))) == -1 ) return false;
+            tiles = tiles.substring(0, index) + tiles.substring(index+1);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(canSpell("quijibo ", "jib"));
+    }
 }
